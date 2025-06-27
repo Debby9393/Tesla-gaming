f6f7064a451ea562f028cdcbacbec1ce6dc309b8
Debby9393 <bakareharuna1@gmail.com>
2025-06-27 05:38:34 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#66fcf1";
+ctx.font = "30px Arial";
+ctx.fillText("TeslaFootball 24 Lite - Demo", 150, 300);
